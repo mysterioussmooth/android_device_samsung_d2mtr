54f25dab0f6daf1b39d5c8ea45943cece16c4901
nocoast <voteforpedro8@gmail.com>
2012-11-14 11:17:44 -0600

    Step one to liquifiying

--- cm.mk
+++ cm.mk
@@ -1,16 +1,14 @@
+# device
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Telephony
+$(call inherit-product, vendor/liquid/config/common_cdma.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="d2mtr-user 4.0.4 IMM76D R530MVQALJ1 release-keys" PRIVATE_BUILD_DESC="samsung/d2mtr/d2mtr:4.0.4/IMM76D/R530MVQALJ1:user/release-keys"
 
-PRODUCT_NAME := cm_d2mtr
+PRODUCT_NAME := liquid_d2mtr
 PRODUCT_DEVICE := d2mtr
 
